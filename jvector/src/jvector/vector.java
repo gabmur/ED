51a5f0ee4b9e4a1b7c3332706d35f934b68bf6c2
gabmur <1dam.gmartinez@institutoserpis.org>
2018-10-18 12:16:48 +0200

    añadido jvector:propuesto indexOf

---
+++ jvector/src/jvector/vector.java
@@ -0,0 +1,17 @@
+package jvector;
+
+public class vector {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] v = new int[] {16,12,15,14,17};
+		int x = 14;
+		int index = indexOf(v,14);
+		System.out.println("index="+index);
+		
+	}
+	public static int indexOf(int[] v, int x) {
+		return 3;
+	}
+
+}
