b811739e6347bb07888814b0efcd1598fd651c63
gabmur <1dam.gmartinez@institutoserpis.org>
2018-11-19 09:45:30 +0100

    añadido CSuma

---
+++ CSuma/CSuma/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSuma
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+           // Console.Write("Primer numero");
+            //string stringPrimerNumero = Console.ReadLine();
+            //int primerNumero = int.Parse(stringPrimerNumero);
+
+            //Console.Write("Segundo  numero");
+            //string stringSegundoNumero = Console.ReadLine();
+            //int segundoNumero = int.Parse(stringSegundoNumero);
+            //int suma = primerNumero + segundoNumero;
+
+           
+            //Console.WriteLine("la suma de ambos numeros es: "+suma);
+
+            decimal primerNumero = readDecimal("Primer numero");
+            decimal segundoNumero = readDecimal("Segundo numero");
+            decimal suma = primerNumero + segundoNumero;
+            Console.WriteLine("suma=" + suma);
+        }
+        private static decimal readDecimal(string label){
+            Console.Write(label);
+            string stringDecimal = Console.ReadLine();
+            return decimal.Parse(stringDecimal);
+        }
+    }
+}
