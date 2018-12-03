a334b68bf73cabd2d4cbeb7ec14e35d009e6651a
gabmur <1dam.gmartinez@institutoserpis.org>
2018-12-03 09:48:10 +0100

    añadido CVector y CGtk

---
+++ CGtk/CGtk/MainWindow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnButton2ButtonPressEvent(object sender, EventArgs e)
+    {
+        string nombre = entryNombre.Text;
+        labelSaludo.Text = "Hola " + nombre;
+
+        //Console.WriteLine("click en buttonAceptar con nombre =" + nombre);
+    }
+
+}
---
+++ CVector/CVector/VectorTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+namespace CVector
+{
+    [TestFixture]
+    public class VectorTest
+    {
+        [Test]
+        public void BinarySearch()
+        {
+            Assert.AreEqual(-1, Vector.BinarySearch(new int[] { 45 }, 33));
+            Assert.AreEqual(5, Vector.BinarySearch(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 6)); 
+            Assert.AreEqual(1, Vector.BinarySearch(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 2));
+            Assert.AreEqual(-1, Vector.BinarySearch(new int[] { 45 }, 33));
+            Assert.AreEqual(-1, Vector.BinarySearch(new int[] { 45 }, 33));
+
+        }
+    }
+}
